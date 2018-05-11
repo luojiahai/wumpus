126cb033b6c7d9db328fe9cb94857adc5de080d9
luojiahai <ffreylaw@gmail.com>
2018-05-11 15:09:06 +1000

    update

--- wumpus.pl
+++ wumpus.pl
@@ -24,18 +24,23 @@ guess(state(X0-Y0,Guess0,Map0), State, Guess) :-
           last(Guess0, LastG),
           guessPartOne(X0, Y0, Map0, LastG, Dirns),
           append(Guess0, Dirns, Guess)
-        ; hasWumpus(X0-Y0, Map0, X-Y) -> 
+        ; hasWumpus(X0-Y0, Map0, X-Y),
           % guess part two: shoot the wumpus
-          writeln(X-Y), halt
+          State = state(X0-Y0,Guess0,Map0),
+          halt
         ).
 
 %% update state
-updateState(state(X0-Y0,_Guess0,Map0), Guess, Feedback, State) :-
-        last(Guess, LastG), last(Feedback, LastF),
-        updatePos(X0-Y0, LastG, LastF, X-Y),
-        updateMap(Map0, X-Y, LastF, Map),
-        State = state(X-Y,Guess,Map),
-        writeln(LastF), writeln(State).
+updateState(state(X0-Y0,_,Map0), Guess, Feedback, State) :-
+        ( \+ hasWumpus(X0-Y0, Map0, _) ->
+          last(Guess, LastG), last(Feedback, LastF),
+          updatePos(X0-Y0, LastG, LastF, X-Y),
+          updateMap(Map0, X-Y, LastF, Map),
+          State = state(X-Y,Guess,Map),
+          writeln(LastF), writeln(State)
+        ; hasWumpus(X0-Y0, Map0, X-Y),
+          writeln(X-Y)
+        ).
 
 guessPartOne(X0, Y0, Map, PrevDir, [Dirn]) :-
         ( PrevDir == east,  Y is Y0 + 1, notVisited(X0-Y, Map) -> Dirn = south
@@ -47,9 +52,6 @@ guessPartOne(X0, Y0, Map, PrevDir, [Dirn]) :-
 
 notVisited(X-Y, [point(X-Y,none)|_]).
 notVisited(X-Y, [point(_,_)|Rest]) :- notVisited(X-Y, Rest).
-
-hasWumpus(X-Y, [point(X-Y,wumpus)|_], X-Y).
-hasWumpus(X-Y, [point(_,_)|Rest], WX-WY) :- hasWumpus(X-Y, Rest, WX-WY).
 
 updatePos(X0-Y0, LastG, LastF, X-Y) :-
         ( LastG == east,  LastF \= wall -> X is X0 + 1, Y is Y0
@@ -64,6 +66,9 @@ updateMap([point(X-Y,_)|Rest], X-Y, LastF, [point(X-Y,LastF)|Rest]) :-
         updateMap(Rest, X-Y, LastF, Rest).
 updateMap([point(I-J,Feedback)|Rest0], X-Y, LastF, [point(I-J,Feedback)|Rest]) :- 
         updateMap(Rest0, X-Y, LastF, Rest).
+
+hasWumpus(X-Y, [point(X-Y,wumpus)|_], X-Y).
+hasWumpus(X-Y, [point(_,_)|Rest], WX-WY) :- hasWumpus(X-Y, Rest, WX-WY).
 
 find(Start, End, Path) :-
         find(Start, End, [Start], Path).
