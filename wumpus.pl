0ce3fa8e9c5d4bb1d7bbcdfba2533d7bbbf2bde5
luojiahai <ffreylaw@gmail.com>
2018-05-11 18:53:15 +1000

    update

--- wumpus.pl
+++ wumpus.pl
@@ -1,10 +1,10 @@
-%%% ----------------------------------------------
-%%% -- | COMP90048 Declarative Programming    | --
-%%% -- | Project 4                            | --
-%%% -- | Name: Geoffrey Ka-Hoi Law            | --
-%%% -- | Student No.: 759218                  | --
-%%% -- | Email: glaw@student.unimelb.edu.au   | --
-%%% ----------------------------------------------
+%%% ---------------------------------------------- %%%
+%%% -- | COMP90048 Declarative Programming    | -- %%%
+%%% -- | Project 4                            | -- %%%
+%%% -- | Name: Geoffrey Ka-Hoi Law            | -- %%%
+%%% -- | Student No.: 759218                  | -- %%%
+%%% -- | Email: glaw@student.unimelb.edu.au   | -- %%%
+%%% ---------------------------------------------- %%%
 
 :- module(wumpus, [initialState/5, guess/3, updateState/4]).
 
@@ -14,66 +14,79 @@ initialState(NR, NC, XS, YS, state(XS-YS,[],Map)) :-
 
 %% initial guess
 guess(state(X0-Y0,[],Map0), State, Guess) :-
-        State = state(X0-Y0,[],Map0),
-        Guess = [east].
+        X1 is X0 + 1, Y1 is Y0,
+        Guess = [east],
+        State = state(X1-Y1,Guess,Map0).
 %% guess
 guess(state(X0-Y0,Guess0,Map0), State, Guess) :-
         ( \+ hasWumpus(X0-Y0, Map0, _) ->
           % guess part one: find the wumpus
-          State = state(X0-Y0,Guess0,Map0),
           last(Guess0, LastG),
-          guessPartOne(X0, Y0, Map0, LastG, Dirns),
-          append(Guess0, Dirns, Guess)
+          guessPartOne(X0-Y0, Map0, LastG, Dirns, X1-Y1),
+          append(Guess0, Dirns, Guess),
+          State = state(X1-Y1, Guess0, Map0)
         ; hasWumpus(X0-Y0, Map0, X-Y),
           % guess part two: shoot the wumpus
-          State = state(X0-Y0,Guess0,Map0),
+          State = state(X0-Y0, Guess0, Map0),
+          writeln(X-Y),
           halt
         ).
 
 %% update state
 updateState(state(X0-Y0,_,Map0), Guess, Feedback, State) :-
         ( \+ hasWumpus(X0-Y0, Map0, _) ->
-          last(Guess, LastG), last(Feedback, LastF),
-          updatePos(X0-Y0, LastG, LastF, X-Y),
-          updateMap(Map0, X-Y, LastF, Map),
-          State = state(X-Y,Guess,Map),
+          last(Feedback, LastF),
+          updateMap(Map0, X0-Y0, LastF, Map),
+          State = state(X0-Y0, Guess, Map),
+          % debug here
           writeln(LastF), writeln(State)
         ; hasWumpus(X0-Y0, Map0, X-Y),
           writeln(X-Y)
         ).
 
-guessPartOne(X0, Y0, Map, PrevDir, [Dirn]) :-
-        ( PrevDir == east,  Y is Y0 + 1, notVisited(X0-Y, Map) -> Dirn = south
-        ; PrevDir == south, X is X0 - 1, notVisited(X-Y0, Map) -> Dirn = west
-        ; PrevDir == west,  Y is Y0 - 1, notVisited(X0-Y, Map) -> Dirn = north
-        ; PrevDir == north, X is X0 + 1, notVisited(X-Y0, Map) -> Dirn = east
-        ; Dirn = east
+pickNone([point(X-Y,none)|_], X-Y).
+pickNone([_|Rest], X-Y) :-
+        pickNone(Rest, X-Y).
+
+guessPartOne(X0-Y0, Map, PrevDir, Dirns, X1-Y1) :-
+        ( PrevDir == east,  Y1 is Y0 + 1, X1 is X0, notVisited(X1-Y1, Map) -> Dirns = [south]
+        ; PrevDir == south, X1 is X0 - 1, Y1 is Y0, notVisited(X1-Y1, Map) -> Dirns = [west]
+        ; PrevDir == west,  Y1 is Y0 - 1, X1 is X0, notVisited(X1-Y1, Map) -> Dirns = [north]
+        ; PrevDir == north, X1 is X0 + 1, Y1 is Y0, notVisited(X1-Y1, Map) -> Dirns = [east]
+        ; pickNone(Map, X1-Y1), setof(Path, generatePath(Map, X0-Y0, X1-Y1, Path), [Dirns|_Rest])
         ).
 
 notVisited(X-Y, [point(X-Y,none)|_]).
 notVisited(X-Y, [point(_,_)|Rest]) :- notVisited(X-Y, Rest).
 
-updatePos(X0-Y0, LastG, LastF, X-Y) :-
-        ( LastG == east,  LastF \= wall -> X is X0 + 1, Y is Y0
-        ; LastG == south, LastF \= wall -> X is X0, Y is Y0 + 1
-        ; LastG == west,  LastF \= wall -> X is X0 - 1, Y is Y0
-        ; LastG == north, LastF \= wall -> X is X0, Y is Y0 - 1
-        ; X is X0, Y is Y0
-        ).
-
 updateMap([], _, _, []).
 updateMap([point(X-Y,_)|Rest], X-Y, LastF, [point(X-Y,LastF)|Rest]) :- 
         updateMap(Rest, X-Y, LastF, Rest).
-updateMap([point(I-J,Feedback)|Rest0], X-Y, LastF, [point(I-J,Feedback)|Rest]) :- 
+updateMap([point(I-J,Type)|Rest0], X-Y, LastF, [point(I-J,Type)|Rest]) :- 
         updateMap(Rest0, X-Y, LastF, Rest).
 
 hasWumpus(X-Y, [point(X-Y,wumpus)|_], X-Y).
 hasWumpus(X-Y, [point(_,_)|Rest], WX-WY) :- hasWumpus(X-Y, Rest, WX-WY).
 
-find(Start, End, Path) :-
-        find(Start, End, [Start], Path).
-find(Start, Start, _Previous, []).
-find(Start, End, Previous, [Dirn|Path]) :-
-        edge(Start, Dirn, Med),
-        \+ member(Med, Previous), % dont visit previous places
-        find(Med, End, [Med|Previous], Path).
+isEdge(_Map, X-Y, X-Y). % the end point
+isEdge([point(X-Y,empty)|_], X-Y, _).   % empty point
+isEdge([point(X-Y,smell)|_], X-Y, _).   % smell point
+isEdge([point(X-Y,stench)|_], X-Y, _).  % stench point
+isEdge([point(_,_)|Rest], X-Y, EX-EY) :- isEdge(Rest, X-Y, EX-EY). % otherwise
+
+through(Map, X0-Y0, Dirn, X-Y, EX-EY) :- 
+        nextPos(X0-Y0, X-Y, Dirn),
+        isEdge(Map, X-Y, EX-EY).
+
+nextPos(X0-Y0, X0-Y, Dirn) :- Y is Y0 + 1, Dirn = south.
+nextPos(X0-Y0, X-Y0, Dirn) :- X is X0 + 1, Dirn = east.
+nextPos(X0-Y0, X0-Y, Dirn) :- Y is Y0 - 1, Dirn = north.
+nextPos(X0-Y0, X-Y0, Dirn) :- X is X0 - 1, Dirn = west.
+
+generatePath(Map, Start, End, Path) :-
+        generatePath(Map, Start, End, [Start], Path).
+generatePath(_Map, Start, Start, _Previous, []).
+generatePath(Map, Start, End, Previous, [Dirn|Path]) :-
+        through(Map, Start, Dirn, Med, End),
+        \+ member(Med, Previous),
+        generatePath(Map, Med, End, [Med|Previous], Path).
